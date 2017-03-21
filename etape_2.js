1a48113efe7a3807c2a8a1c197d65c61ce81241f
Grenier Alexandre <a.grenier12@gmail.com>
2017-03-21 15:55:42 -0400

    #2 fonction terminée

---
+++ etape_2.js
@@ -0,0 +1,21 @@
+var obj;
+
+fs.readFile('public/text/collection_provinces.json', function (err, data){
+	if(err) return console.error(err);
+	obj = JSON.parse(data);
+	console.log(obj.toString());
+});
+
+function etape2(){
+	 var chaine = "";
+
+	chaine = chaine+"<table style='border-right: 1px solid black'>";
+	for (var i = 0; i <= obj.length-1; i++) {
+
+		chaine = chaine+"<tr><td>" + obj[i][0].toString() + "</td><td>" + obj[i][1].toString()+ "</td><td>"+ obj[i][2].toString()+"</td></tr>";
+	}
+	chaine = chaine+"</table>";
+
+	return chaine;
+}
+
